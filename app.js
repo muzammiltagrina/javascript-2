391148acc717919f62d897aca945f3b073f2422a
muhammad muzammil <muzammiltagrina@gmail.com>
2024-12-19 19:19:24 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,92 @@
+alert("Welcome to JS Assignment # 2");
+alert("Chapter - 4 , 5 & 6");
+// chapter start //
+document.write("<h1>Chapter-4</h1>");
+// chapter - 4 /
+// Question - 1 //
+document.write("<h2>Rules For Naming JS Variables</h2>");
+document.write("<p>Variable names can only contain Numbers, $,and _.For example $my_1stVariable</p>");
+document.write("<p>Variables must begin with a Letter, $ or_. For example $name, _name or name</p>");
+document.write("<p>Variable names are case Sensitive</p>");
+document.write("<p>Variable names should not be JS Keywords</p>");
+// chpter start //
+document.write("<h1>Chapter-5</h1>");
+// Chapter - 5 //
+// Question - 1 //
+document.write("<h2>Sum OF 3 & 5 is 8</h2>");
+document.write("<p>Value After Variable Is Undefined</p>");
+document.write("<p>Initial Value : 5 </p>");
+document.write("<p>Value after increment is : 6</p>");
+document.write("<p>Value after addition is : 13</p>");
+document.write("<p>Value after decrement is : 12</p>");
+document.write("<p>The reminder is : 0</p>");
+
+// Question - 2 //
+document.write("<h2>Total cost to buy 5 tickets to a Movie is 3000 PKR</h2>");
+document.write("<h2>Table Of 04</h2>");
+document.write("<p>4x1=4</p>");
+document.write("<p>4x2=8</p>");
+document.write("4x3=12");
+document.write("<p>4x4=16</p>");
+document.write("<p>4x5=20</p>");
+document.write("<p>4x6=24</p>");
+document.write("<p>4x7=28</p>");
+document.write("<p>4x8=32</p>");
+document.write("<p>4x9=36</p>");
+document.write("<p>4x10=40</p>");
+ 
+// chapter Start //
+document.write("<h1>Chapter-6</h1>");
+// Chapter - 6 //
+// Question - 1 //
+document.write("<h2>25oC is 77oF.</h2>")
+document.write("<h2>70oF is 21.1111111111111oC.</h2>")
+
+// Question - 2 //
+document.write("<h2>Shopping Cart</h2>")
+document.write("<p>Price of Item 1 : 650</p>")
+document.write("<p>Quantity of item is 3</p>")
+document.write("<p>Price of item 2 is 7</p>")
+document.write("<p>Quantity of Items</p>")
+document.write("<p>Shipping Charges 100</p>")
+document.write("<h3> Total Cost Of Your Order Is : 2750</h3>")
+
+// Questuon - 3  //
+document.write("<h2>Marks Sheet</h2>")
+document.write("<p>Total Percentsge 980</p>")
+document.write("<p>Marks Obtained 804</p>")
+document.write("<p>Percentage Obtained 82.040816309620</p>")
+
+// Question - 4 //
+document.write("<h2>currancy in PKR</h2>")
+document.write("<p>Total Currancy In PKR is : 1748</p>")
+
+// Question - 5 //
+document.write("<h2>Age Calculator</h2>")
+document.write("<p>Current Year 2024</p>")
+document.write("<p>Birth year : 2008</p>")
+document.write("<p>You age 16</p>")
+
+// Question - 6 //
+document.write("<h2>The Geometrizers</h2>")
+document.write("<p>Radius of a circle : 20</p>")
+document.write("<p>The Circumference is : 125.6799999999999</p>")
+document.write("<p>The area is : 1256.8</p>")
+   
+// chapter start //
+document.write("<h1>Chapter-7</h1>")
+// Chapter - 7 //
+// Question - 1 //
+document.write("<h2>Result</h2>")
+document.write("<p>The value of a is : 10</p>")
+document.write("<p>......................</p>")
+document.write("<p>The Value of ++a is : 11</p>")
+document.write("<p>Now Value of a is : 11</p>")
+document.write("<p>The Value of ++a is : 11</p>")
+document.write("<p>Now Value of a is : 12</p>")
+document.write("<p>The Value of ++a is : 11</p>")
+document.write("<p>Now Value of a is : 11</p>")
+document.write("<p>The Value of ++a is : 11</p>")
+document.write("<p>Now Value of a is : 10</p>")
+
+
